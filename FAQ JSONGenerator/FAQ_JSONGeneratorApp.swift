b75df76ab91bca7b433d9f0b21f470b174325f99
Stewart Lynch <slynch@createchsol.com>
2024-02-01 11:37:28 -0800

    Initial Commit

---
+++ FAQ JSONGenerator/FAQ_JSONGeneratorApp.swift
@@ -0,0 +1,22 @@
+//
+// Created for FAQ JSONGenerator
+// by  Stewart Lynch on 2024-02-01
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct FAQ_JSONGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
